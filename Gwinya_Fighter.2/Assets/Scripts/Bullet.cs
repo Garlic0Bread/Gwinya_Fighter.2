e0a7a8a693628d98bbdd50bb4c11c4118aa6ec76
Owl <ntshabangu208@gmail.com>
2023-05-20 23:11:07 +0200

    .

--- Gwinya_Fighter.2/Assets/Scripts/Bullet.cs
+++ Gwinya_Fighter.2/Assets/Scripts/Bullet.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private int damage = 3;
+    [SerializeField] private Collider2D colliderToIgnore;
 
+    private void Start()
+    {
+        GameObject playerCollider = GameObject.FindGameObjectWithTag("Player");
+        colliderToIgnore = playerCollider.GetComponent<Collider2D>();
+    }
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if(collider.GetComponent<Health>() != null)
         {
-            Health health = collider.GetComponent<Health>();
-            health.Damage(damage);
-            Destroy(gameObject);
+            if (collider.CompareTag("Player"))
+            {
+                Physics2D.IgnoreCollision(GetComponent<Collider2D>(), colliderToIgnore);
+            }
+            else
+            {
+                Health health = collider.GetComponent<Health>();
+                health.Damage(damage);
+                Destroy(gameObject);
+            }
         }
     }
 }
