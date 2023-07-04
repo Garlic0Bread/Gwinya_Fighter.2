7deaf8fc0dc2bc8102da6dd19e3c3103b324785c
Garlic-Bread <ntshabangu208@gmail.com>
2023-07-04 06:42:44 +0200

    Revert "Dungeon Explorer"
    This reverts commit 08659e3bd2513731fa4e47eb0547e47ef38ce1b1.

--- Gwinya_Fighter.2/Assets/Scripts/Bullet.cs
+++ Gwinya_Fighter.2/Assets/Scripts/Bullet.cs
@@ -25,7 +25,6 @@ public class Bullet : MonoBehaviour
                 Health health = collider.GetComponent<Health>();
                 health.Damage(damage);
                 Destroy(gameObject);
-
             }
         }
     }
