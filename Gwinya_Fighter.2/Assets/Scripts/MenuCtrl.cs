dd720a5c59a37f9be7db60943c37d35a9fc60d66
StealthyMeat59 <76598912+StealthyMeat59@users.noreply.github.com>
2023-07-24 18:06:03 +0200

    Menu
    1st Iteration of the menu.

---
+++ Gwinya_Fighter.2/Assets/Scripts/MenuCtrl.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuCtrl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+
+    public void Changer()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void Exit()
+    {
+        Application.Quit ();
+    }
+}
